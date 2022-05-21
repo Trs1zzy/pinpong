0f36cb70712b9d4682b8dc35b3996215ed5fd5e5
Trs1zzy <105965534+Trs1zzy@users.noreply.github.com>
2022-05-21 11:01:21 +0300

    Update pinpong.py
    добавлены ракетки

--- pinpong.py
+++ pinpong.py
@@ -12,11 +12,80 @@ finish = False
 clock = time.Clock()
 FPS = 60
  
+
+
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #Вызываем конструктор класса (Sprite):
+        sprite.Sprite.__init__(self)
+ 
+       #каждый спрайт должен хранить свойство image - изображение
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+ 
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+       #метод, отрисовывающий героя на окне
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+    
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 105:
+            self.rect.y += self.speed
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 105:
+            self.rect.y += self.speed
+
+
+
+
+raketka1 = Player('nonavw.png', 580, 200, 25, 100, 5) 
+
+
+
+
+
+raketka2 = Player('nonavw.png', 0, 200, 25, 100, 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 while game:
-   for e in event.get():
-       if e.type == QUIT:
-           game = False  
-   if finish != True:
-       window.fill(back)
-   display.update()
-   clock.tick(FPS)
+    for e in event.get():
+        if e.type == QUIT:
+            game = False  
+    if finish != True:
+        window.fill(back)
+        raketka1.update_l()
+        raketka2.update_r()
+        raketka2.reset()
+        raketka1.reset()
+
+    display.update()
+    clock.tick(FPS)
